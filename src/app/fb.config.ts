b587be1ebee45d6eb13645b6e977c517961e4f84
RustCerebri <klopixol89@gmail.com>
2021-03-11 13:38:23 +0300

    refactoring

---
+++ src/app/fb.config.ts
@@ -0,0 +1,10 @@
+export const FB_CONFIG = {
+  apiKey: "AIzaSyDDOlrkE7XcNCkG96Yx3bqkDz_JJmEClOw",
+  authDomain: "angular-task-68d08.firebaseapp.com",
+  databaseURL: "https://angular-task-68d08-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "angular-task-68d08",
+  storageBucket: "angular-task-68d08.appspot.com",
+  messagingSenderId: "472100978769",
+  appId: "1:472100978769:web:368a85493300b36fcdf870",
+  fbDbUrl: 'https://angular-task-68d08-default-rtdb.europe-west1.firebasedatabase.app'
+};
